3a3d79906eae6fa690729b0dc4e1bb7cc4d2c288
tiago <tiagozhk@gmail.com>
2017-04-05 17:19:05 -0300

    Implementing basic cache

--- app/src/main/java/com/example/tgzoom/letswatch/network/NetworkModule.java
+++ app/src/main/java/com/example/tgzoom/letswatch/network/NetworkModule.java
@@ -7,9 +7,13 @@ import com.example.tgzoom.letswatch.AppModule;
 import com.facebook.stetho.Stetho;
 import com.facebook.stetho.okhttp3.StethoInterceptor;
 
+import java.io.File;
+import java.util.UUID;
+
 import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.Cache;
 import okhttp3.OkHttpClient;
 
 /**
@@ -23,10 +27,33 @@ import okhttp3.OkHttpClient;
 )
 public class NetworkModule {
 
+    private static long MAX_CACHE_SIZE = 1024;
+
     @Singleton
     @Provides
-    OkHttpClient provideOkHttpClient(){
-        OkHttpClient okHttpClient = new OkHttpClient.Builder().addNetworkInterceptor(new StethoInterceptor()).build();
+    OkHttpClient provideOkHttpClient(Cache cache){
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addNetworkInterceptor(new StethoInterceptor())
+                .cache(cache)
+                .build();
         return okHttpClient;
+    }
+
+    @Singleton
+    @Provides
+    Cache provideOkHttpCache(File file, long maxCacheSize){
+        return new Cache(file,maxCacheSize);
+    }
+
+    @Singleton
+    @Provides
+    File provideFile(){
+        return new File(System.getProperty("java.io.tmpdir"), UUID.randomUUID().toString());
+    }
+
+    @Singleton
+    @Provides
+    long provideMaxCacheSize(){
+        return MAX_CACHE_SIZE;
     }
 }
